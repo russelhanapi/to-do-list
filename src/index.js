67c87cc74030c4451c81e95dee1ba9633c122e5d
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-02-19 23:35:31 +0800

    feat: Add utility functions for modal handling and object creation
    - Implemented `exitModal()` to reset and close the modal.
    - Created `createTodo()` and `createProject()` functions for structured object creation.

--- src/index.js
+++ src/index.js
@@ -24,3 +24,31 @@ const projectsArr = [
   { title: "home", todoArr: [] },
   { title: "work", todoArr: [] },
 ];
+
+// NOTE: UTILITY FUNCTIONS
+const exitModal = () => {
+  formModal.reset();
+  modal.close();
+};
+
+const createTodo = (
+  id,
+  title,
+  description,
+  dueDate = "N/A",
+  priority,
+  project,
+  isCompleted = "false",
+  isDeleted = "false"
+) => ({
+  id,
+  title,
+  description,
+  dueDate,
+  priority,
+  project,
+  isCompleted,
+  isDeleted,
+});
+
+const createProject = (title, todoArr = []) => ({ title, todoArr });
