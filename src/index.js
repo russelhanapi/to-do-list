0384b0ffd3e4ccdd1a0f8cfb1192c0a826eb1d88
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-02-19 23:33:37 +0800

    feat: Select DOM elements and initialize state management
    - Defined constants for all interactive DOM elements.
    - Created arrays to manage todos, completed todos, deleted todos, and projects.
    - Initialized default projects: Home and Work.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+"use-strict";
+
 import "./style.css";
 import "material-icons/iconfont/material-icons.css";
+
+// NOTE: DOM ELEMENTS
+const btnAddProject = document.querySelector(".btn-add-project");
+const btnAddTodo = document.querySelector(".btn-add-todo");
+const btnDeleteTodo = document.querySelector(".btn-delete-todo");
+const btnCompleteTodo = document.querySelector(".btn-complete-todo");
+const btnFormCancel = document.querySelector(".btn-form-cancel");
+const btnFormSubmit = document.querySelector(".btn-form-submit");
+const containerTodos = document.querySelector(".todos-container");
+const modal = document.querySelector(".modal-action-todo");
+const formModal = document.querySelector(".modal-form");
+const listProjects = document.querySelector(".todo-list-projects");
+
+// NOTE: STATE MANAGEMENT
+const todoArr = [];
+
+const deletedTodoArr = [];
+const completedArr = [];
+const projectsArr = [
+  { title: "home", todoArr: [] },
+  { title: "work", todoArr: [] },
+];
